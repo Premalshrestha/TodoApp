af4f65d682719c20852b3708efce08ab5d44eac3
premalshrestha <premalshrestha00@gmail.com>
2025-07-30 23:22:55 +0545

    this is the final commit

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Task
 from .serializers import TaskSerializer
+from django.core.mail import send_mail
+
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
@@ -9,7 +12,30 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-    
+        task = serializer.save(user=self.request.user)
+        # Send email
+        send_mail(
+            subject='New Task Created',
+            message=f'Your task "{task.title}" is created and due on {task.due_date}.',
+            from_email='you@example.com',
+            recipient_list=[self.request.user.email],
+            fail_silently=False,
+        )
+
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+    def perform_update(self, serializer):
+        task = serializer.save()
+
+        # Optional: send update email
+        send_mail(
+            subject='Task Updated',
+            message=f'Your task "{task.title}" was updated. New status: {task.status}.',
+            from_email='you@example.com',
+            recipient_list=[self.request.user.email],
+            fail_silently=False,
+        )
+
+
+
