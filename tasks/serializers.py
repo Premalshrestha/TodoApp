bd65ac1907603de717e125bc3241016ca7415184
premalshrestha <premalshrestha00@gmail.com>
2025-07-30 22:55:49 +0545

    The categorical data is added

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
 
 class TaskSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True
+    )
     class Meta:
         model = Task
-        fields = '__all__'
-        read_only_fields = ['user','created_at']
+        fields = [
+            'id', 'user', 'title', 'description', 'due_date', 
+            'status', 'attachment', 'created_at', 'category', 'category_id'
+        ]
+        
