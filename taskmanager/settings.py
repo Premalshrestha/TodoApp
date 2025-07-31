e180b0f1e3ee53c6828a28ecdf0fdb1a1f70f2d6
Premalshrestha <87117722+Premalshrestha@users.noreply.github.com>
2025-07-31 09:25:36 +0545

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'premalshrestha00@gmail.com'
-EMAIL_HOST_PASSWORD = 'boea gbro vsbf xhts'  # Use app password (not your real Gmail password!)
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''  # Use app password (not your real Gmail password!)
 
 
